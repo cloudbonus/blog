5846cd42353c739df7595d6b955e314a44c66a23
Raman Haurylau <91277781+cloudbonus@users.noreply.github.com>
2024-04-11 17:29:55 +0300

    fix/WE-8 - Fix wildcard imports

--- src/main/java/com/github/blog/dao/impl/UserDaoImpl.java
+++ src/main/java/com/github/blog/dao/impl/UserDaoImpl.java
@@ -7,7 +7,12 @@ import com.github.blog.util.DefaultConnectionHolder;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
