6e51431f225d78f69c3fd4d3e1022ad9c4d80543
Raman Haurylau <91277781+cloudbonus@users.noreply.github.com>
2024-04-18 21:13:25 +0300

    refactor/WF-5 - Delete files from previous homework

--- src/main/java/com/github/blog/dao/UserDetailsDao.java
+++
@@ -1,9 +0,0 @@
-package com.github.blog.dao;
-
-import com.github.blog.model.UserDetails;
-
-/**
- * @author Raman Haurylau
- */
-public interface UserDetailsDao extends CrudDao<UserDetails, Integer> {
-}
