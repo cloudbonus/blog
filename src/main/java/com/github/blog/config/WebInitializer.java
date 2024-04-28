6e2e5c0baf65a4e533084e457dff77e5c18116a9
Raman Haurylau <91277781+cloudbonus@users.noreply.github.com>
2024-04-28 22:54:49 +0300

    update/WG-4 - Update configs

---
+++ src/main/java/com/github/blog/config/WebInitializer.java
@@ -0,0 +1,23 @@
+package com.github.blog.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Raman Haurylau
+ */
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+}
---
+++ src/test/java/com/github/blog/controller/WebTestConfig.java
@@ -0,0 +1,36 @@
+package com.github.blog.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import javax.sql.DataSource;
+
+
+/**
+ * @author Raman Haurylau
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.github.blog")
+public class WebTestConfig {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>("postgres:latest");
+    }
+
+    @Bean
+    public DataSource dataSource(final PostgreSQLContainer<?> container) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(container.getDriverClassName());
+        dataSource.setUrl(container.getJdbcUrl());
+        dataSource.setUsername(container.getUsername());
+        dataSource.setPassword(container.getPassword());
+        return dataSource;
+    }
+}
