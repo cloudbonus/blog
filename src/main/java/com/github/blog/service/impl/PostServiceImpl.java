1c56d973ecbc7bed3004adf50b449d2f549626ec
Raman Haurylau <91277781+cloudbonus@users.noreply.github.com>
2024-05-21 19:00:51 +0300

    add/WI-12 - Add role check in a create method

--- src/main/java/com/github/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/github/blog/service/impl/PostServiceImpl.java
@@ -5,15 +5,19 @@ import com.github.blog.controller.dto.request.PageableRequest;
 import com.github.blog.controller.dto.request.PostRequest;
 import com.github.blog.controller.dto.request.filter.PostDtoFilter;
 import com.github.blog.controller.dto.response.Page;
+import com.github.blog.model.Order;
 import com.github.blog.model.Post;
+import com.github.blog.repository.OrderDao;
 import com.github.blog.repository.PostDao;
 import com.github.blog.repository.dto.common.Pageable;
 import com.github.blog.repository.dto.filter.PostFilter;
 import com.github.blog.service.PostService;
-import com.github.blog.service.exception.PostErrorResult;
+import com.github.blog.service.exception.ExceptionEnum;
 import com.github.blog.service.exception.impl.PostException;
 import com.github.blog.service.mapper.PageableMapper;
 import com.github.blog.service.mapper.PostMapper;
+import com.github.blog.service.statemachine.state.OrderState;
+import com.github.blog.service.util.PostAccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,22 +29,34 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
-
     private final PostDao postDao;
     private final PostMapper postMapper;
     private final PageableMapper pageableMapper;
+    private final OrderDao orderDao;
+    private final PostAccessHandler postAccessHandler;
 
     @Override
     public PostDto create(PostRequest request) {
         Post post = postMapper.toEntity(request);
-        return postMapper.toDto(postDao.create(post));
+        post = postDao.create(post);
+
+        if (postAccessHandler.hasRole("ROLE_COMPANY")) {
+            Order order = new Order();
+            order.setPost(post);
+            order.setUser(post.getUser());
+            order.setState(OrderState.NEW.name());
+
+            orderDao.create(order);
+        }
+
+        return postMapper.toDto(post);
     }
 
     @Override
     public PostDto findById(Long id) {
         Post post = postDao
                 .findById(id)
-                .orElseThrow(() -> new PostException(PostErrorResult.POST_NOT_FOUND));
+                .orElseThrow(() -> new PostException(ExceptionEnum.POST_NOT_FOUND));
 
         return postMapper.toDto(post);
     }
@@ -53,7 +69,7 @@ public class PostServiceImpl implements PostService {
         Page<Post> posts = postDao.findAll(dtoFilter, pageable);
 
         if (posts.isEmpty()) {
-            throw new PostException(PostErrorResult.POSTS_NOT_FOUND);
+            throw new PostException(ExceptionEnum.POSTS_NOT_FOUND);
         }
 
         return posts.map(postMapper::toDto);
@@ -63,10 +79,9 @@ public class PostServiceImpl implements PostService {
     public PostDto update(Long id, PostRequest request) {
         Post post = postDao
                 .findById(id)
-                .orElseThrow(() -> new PostException(PostErrorResult.POST_NOT_FOUND));
+                .orElseThrow(() -> new PostException(ExceptionEnum.POST_NOT_FOUND));
 
         post = postMapper.partialUpdate(request, post);
-        post = postDao.update(post);
 
         return postMapper.toDto(post);
     }
@@ -75,8 +90,10 @@ public class PostServiceImpl implements PostService {
     public PostDto delete(Long id) {
         Post post = postDao
                 .findById(id)
-                .orElseThrow(() -> new PostException(PostErrorResult.POST_NOT_FOUND));
+                .orElseThrow(() -> new PostException(ExceptionEnum.POST_NOT_FOUND));
+
         postDao.delete(post);
+
         return postMapper.toDto(post);
     }
 }
