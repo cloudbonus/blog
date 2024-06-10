cfcae07a36156a772515ee2e80e59e0734d2e554
Raman Haurylau <91277781+cloudbonus@users.noreply.github.com>
2024-06-10 11:42:34 +0300

    update/WJ-3 - Remove unnecessary configs due to migration to Spring Boot

--- src/main/java/com/github/blog/config/ApplicationConfig.java
+++ src/main/java/com/github/blog/config/ApplicationConfig.java
@@ -1,29 +1,21 @@
 package com.github.blog.config;
 
-
 import com.github.blog.service.statemachine.event.OrderEvent;
 import com.github.blog.service.statemachine.event.UserInfoEvent;
 import com.github.blog.service.statemachine.state.OrderState;
 import com.github.blog.service.statemachine.state.UserInfoState;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.persist.StateMachineRuntimePersister;
 import org.springframework.statemachine.service.DefaultStateMachineService;
 import org.springframework.statemachine.service.StateMachineService;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author Raman Haurylau
  */
-@EnableWebMvc
 @Configuration
-@ComponentScan(basePackages = "com.github.blog")
-@PropertySource(value = "classpath:application.properties", encoding = "UTF-8")
-public class WebAppConfig {
+public class ApplicationConfig {
 
     @Bean
     public StateMachineService<OrderState, OrderEvent> orderPersister(
@@ -37,10 +29,5 @@ public class WebAppConfig {
             StateMachineFactory<UserInfoState, UserInfoEvent> stateMachineFactory,
             StateMachineRuntimePersister<UserInfoState, UserInfoEvent, String> stateMachinePersister) {
         return new DefaultStateMachineService<>(stateMachineFactory, stateMachinePersister);
-    }
-
-    @Bean
-    public MethodValidationPostProcessor methodValidationPostProcessor() {
-        return new MethodValidationPostProcessor();
     }
 }
--- src/main/java/com/github/blog/config/security/SecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.github.blog.config.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * @author Raman Haurylau
- */
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
