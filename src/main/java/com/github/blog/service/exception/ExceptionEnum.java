68ad05709e6afcfa0eac0452dd5dd5873c826f2b
Raman Haurylau <91277781+cloudbonus@users.noreply.github.com>
2024-06-28 10:58:25 +0300

    update/WK-9 - Update util files, BlogApplication.java, ExceptionEnum.java

--- src/main/java/com/github/blog/service/exception/ExceptionEnum.java
+++ src/main/java/com/github/blog/service/exception/ExceptionEnum.java
@@ -10,10 +10,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ExceptionEnum {
+    // Endpoint
+    ENDPOINT_EXCEPTION(HttpStatus.BAD_REQUEST, "An error occurred while processing request"),
     // Jwt
     AUTHENTICATION_TOKEN_EXCEPTION(HttpStatus.NOT_ACCEPTABLE, "Please verify the validity of your token/account"),
-    // State Machine
-    STATE_TRANSITION_EXCEPTION(HttpStatus.BAD_REQUEST, "Unable to change state"),
     // Auth
     AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "Authentication failed"),
     BAD_CREDENTIALS(HttpStatus.UNAUTHORIZED, "You have entered either the Username and/or Password incorrectly"),
@@ -22,9 +22,14 @@ public enum ExceptionEnum {
     USERS_NOT_FOUND(HttpStatus.NOT_FOUND, "No users could be found"),
     //  UserInfo
     USER_INFO_NOT_FOUND(HttpStatus.NOT_FOUND, "No user info could be found"),
+    // Payment
+    PAYMENT_NULL_EXCEPTION(HttpStatus.BAD_REQUEST, "The payment is null"),
+    PAYMENT_EXCEPTION(HttpStatus.BAD_REQUEST, "The payment has not been received"),
     //  Order
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "Order not found"),
     ORDERS_NOT_FOUND(HttpStatus.NOT_FOUND, "No orders could be found"),
+    ORDER_PROCESSING_EXCEPTION(HttpStatus.BAD_REQUEST, "The order is already being processed"),
+    ORDER_CANCELLATION_EXCEPTION(HttpStatus.BAD_REQUEST, "Only orders that are being processed can be cancelled"),
     //  Tag
     TAG_NOT_FOUND(HttpStatus.NOT_FOUND, "Tag not found"),
     TAGS_NOT_FOUND(HttpStatus.NOT_FOUND, "No tags could be found"),
